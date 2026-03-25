5c0108ffad9f21e332aab35b451e90cc1c5b1099
Aditya-Tiwari2005 <adityatiwarii.work@gmail.com>
2026-03-26 02:21:21 +0530

    new project

---
+++ Basics/Project7/card.js
@@ -0,0 +1,19 @@
+let profile=document.getElementById('profile')
+const button=document.getElementById('Generate')
+button.addEventListener('click',()=>{
+    const requestURl='https://api.github.com/users/Aditya-Tiwari2005'
+    const Xhr=new XMLHttpRequest();
+    Xhr.open('GET',requestURl)
+    Xhr.onreadystatechange=function(){
+          console.log(Xhr.readyState);
+          if (Xhr.readyState === 4) {
+            const data= JSON.parse(this.responseText)
+            profile.innerHTML=`
+            <img src="${data.avatar_url}" height="300" style="border-radius:20%" >
+            <h2>${data.name}</h2>
+            <p>Followers: ${data.followers}</p>`
+          }
+    }
+    Xhr.send()
+})
+
